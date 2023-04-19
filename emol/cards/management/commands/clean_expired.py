575aa46102efd09cee189afd6373a99051a86bf6
Larry Tremblay <lrt512@gmail.com>
2023-04-18 22:44:58 -0400

    Add management command to clean up expired update codes
    Also make queries timezone aware for update codes and send reminders

--- emol/cards/management/commands/clean_expired.py
+++ emol/cards/management/commands/clean_expired.py
@@ -5,7 +5,7 @@ import pytz
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from cards.models.reminder import Reminder
+from cards.models import UpdateCode
 from cards.utility.time import DATE_FORMAT
 
 logger = logging.getLogger("cards")
@@ -16,18 +16,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = timezone.now().astimezone(pytz.utc)
-        due_reminders = Reminder.objects.filter(due_date__lte=now)
-        count = due_reminders.count()
+        expired_codes = UpdateCode.objects.filter(expires_at__lte=now)
+        count = expired_codes.count()
         logger.info(
-            f"Reminders for {date.today().strftime(DATE_FORMAT)}: Found {count} to send"
+            f"Clean up self-serve codes ({date.today().strftime(DATE_FORMAT)}): Found {count}"
         )
 
         if count == 0:
             return
 
-        for reminder in due_reminders:
-            if reminder.should_send_email:
-                if reminder.send_email():
-                    reminder.delete()
-
-        logger.info("Reminders sent")
+        expired_codes.delete()
