d81cdb3c7467599476dca2b57ba5341afb006733
Larry Tremblay <lrt512@gmail.com>
2024-04-16 23:17:27 -0400

    Add basenames to API router registrations

--- emol/cards/api/urls.py
+++ emol/cards/api/urls.py
@@ -9,17 +9,17 @@ from .privacy import ResendPrivacyView
 from .waiver import WaiverViewSet
 
 api_router = routers.SimpleRouter()
-api_router.register(r"combatant-list", CombatantListViewSet)
-api_router.register(r"combatant", CombatantViewSet)
+api_router.register(r"combatant-list", CombatantListViewSet, basename="combatant-list")
+api_router.register(r"combatant", CombatantViewSet, basename="combatant")
 api_router.register(
-    r"combatant-authorization/(?P<discipline>[-\w]+)", CombatantAuthorizationViewSet
+    r"combatant-authorization/(?P<discipline>[-\w]+)", CombatantAuthorizationViewSet, basename="combatant-authorization"
 )
 api_router.register(
-    r"combatant-warrant/(?P<discipline>[-\w]+)", CombatantWarrantViewSet
+    r"combatant-warrant/(?P<discipline>[-\w]+)", CombatantWarrantViewSet, basename="combatant-warrant"
 )
-api_router.register(r"combatant-cards", CardViewSet)
-api_router.register(r"waiver", WaiverViewSet)
-api_router.register(r"card-date", CardDateViewSet)
+api_router.register(r"combatant-cards", CardViewSet, basename="combatant-cards")
+api_router.register(r"waiver", WaiverViewSet, basename="waiver")
+api_router.register(r"card-date", CardDateViewSet, basename="card-date")
 
 # We have some non-model API views, so let's create urlpatterns manaully
 urlpatterns = [
